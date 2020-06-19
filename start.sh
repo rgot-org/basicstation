567a8504723dc7b8eef4a58875ffc1f278a52a2b
mpous <marc@balena.io>
2020-06-19 20:15:39 +0200

    tc.uri coming from balenaCloud variable

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 cd examples/live-s2.sm.tc
-RADIODEV=/dev/spidev0.0 ../../build-rpi-std/bin/station
+echo $TC_URI > tc.uri
+curl https://letsencrypt.org/certs/trustid-x3-root.pem.txt -o tc.trust
+RADIODEV=/dev/spidev0.0 ../../build-rpi-std/bin/station 
 
